2856291e8b098e18164714606a8d984abc89e074
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2022-03-29 22:20:22 +0700

    [Component] Modify Layout Component

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,8 @@
 import React from 'react'
-import Head from 'next/head';
 
 export default function Layout({children}) {
   return (
     <>
-      <Head>
-        <title>Auto Spray IoT</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
       <div className="flex flex-col w-full min-h-screen bg-gray-50">
         {children}
       </div>
